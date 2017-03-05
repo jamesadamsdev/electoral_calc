8ed42ad2eb7ec37c4a2480ef66706d6536b2fbcb
James Adams <jamestadams88@gmail.com>
2017-03-05 17:07:18 -0600

    Finished D'Hondt method and added a simple executable to test

--- backend/electoral_calc_backend/electoral_calc_backend.py
+++ backend/electoral_calc_backend/electoral_calc_backend.py
@@ -5,6 +5,8 @@ Documentation, License etc.
 '''
 
 from flask import Flask, request, abort
+from operator import itemgetter
+from collections import Counter
 import json, logging
 APP = Flask(__name__)
 
@@ -21,6 +23,8 @@ def hello_world():
 
 @APP.route('/calculate', methods=['GET', 'POST'])
 def calculate():
+    # The list of possible counting methods and associated python functions are in the methods.json file.
+    # The logic will check the specified method in the request against the file to get the proper function.
     config_file = open("methods.json")
         
     config = json.loads(config_file.read())
@@ -29,16 +33,47 @@ def calculate():
         return """
 Provide this json in request:
 {
-    "CountMethod": <one of """ + str(list(config.keys())) + """>,
+    "CountMethod": <one of """ + str(list(config.keys())).replace("[", "").replace("]","") + """>,
     "TotalVotes": <int>,
     "NumberOfSeats": <int>,
+    ["Threshold":<int>,]
     "PartyVotes": 
     [
-        {"<Party1>": <int>},
-        {"<Party2>": <int>},
+        {
+            "PartyName":"<Party1>",
+            "Votes": <int>
+        },
+        {
+            "PartyName":"<Party2>",
+            "Votes": <int>
+        },
         ...
-        {"<PartyN>": <int>}
+        {
+            "PartyName":"<PartyN>",
+            "Votes": <int>
+        },
     ]
+}
+
+Response:
+{
+    "PartySeats": 
+    [
+        {
+            "PartyName":"<Party1>",
+            "Seats": <int>
+        },
+        {
+            "PartyName":"<Party2>",
+            "Seats": <int>
+        },
+        ...
+        {
+            "PartyName":"<PartyN>",
+            "Seats": <int>
+        }
+    ]
+    "UnassignedSeats": <int>
 }
 """
     if request.method == 'POST':
@@ -47,12 +82,16 @@ Provide this json in request:
         
         _verify_calculate_data(data)
         
+        # Threshold is optional, but to simplify calculation, set it to 0 if not in request
+        if "Threshold" not in data.keys():
+            data["Threshold"] = 0
+        
         method = data["CountMethod"]
         
         if(method not in config.keys()):
             abort(403)
         
-        return globals()[config[method]](data["TotalVotes"], data["NumberOfSeats"], data["PartyVotes"]), 200
+        return json.dumps(globals()[config[method]](data["TotalVotes"], data["NumberOfSeats"], data["Threshold"], data["PartyVotes"])), 200
         
     abort(404)  
     
@@ -62,8 +101,39 @@ def _verify_calculate_data(data):
         abort(403)
     
 
-def _dhondt(total_votes, number_of_seats, party_votes):
-    return "hi"
+def _dhondt(total_votes, number_of_seats, threshold, party_votes):
+    
+    vote_list = []
+
+    for party in party_votes:
+        party_name = party["PartyName"]
+        votes = party["Votes"]
+        
+        # Ensure party got enough votes, according to the threshold
+        if(votes > (threshold / 100) * total_votes):
+            initial_divisor = 1
+            
+            for i in range(0, number_of_seats):
+                vote_list.append({"PartyName": party_name, "DividedVotes": votes / (initial_divisor + i)})
+    
+    vote_list = sorted(vote_list, key=itemgetter('DividedVotes'), reverse=True)[:number_of_seats] 
+    
+    c = Counter([p["PartyName"] for p in vote_list])
+    
+    res = {"PartySeats":[],"UnassignedSeats":0}
+    
+    unassigned_seats = number_of_seats
+    
+    for party in party_votes:
+        party_name = party["PartyName"]
+        
+        unassigned_seats -= c[party_name]
+        
+        res["PartySeats"].append({"PartyName": party_name, "Seats": c[party_name]})
+        
+    res["UnassignedSeats"] = unassigned_seats
+    
+    return res
                         
 
 if __name__ == '__main__':
